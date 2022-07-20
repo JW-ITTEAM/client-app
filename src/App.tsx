813f6f01cc2f657fd6cc5fd20cb8bcf75be5b616
JW-ITTEAM <it@jamesworldwide.com>
2022-07-20 14:45:42 -0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test1111222
+          Learn React
         </a>
       </header>
     </div>
